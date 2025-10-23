981a9fbe017ae3a85d7c847d3fcc735b037c8e93
MirceaOvidiu <mircea.ovidiu.03@gmail.com>
2025-10-23 13:44:48 +0300

    initial commit

---
+++ wear/src/main/java/com/example/demo/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.demo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.wear.compose.material.Text
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text("Hello World!")
+            }
+        }
+    }
+}
